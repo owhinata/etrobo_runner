11bbf8888c6402ef1abccdc8c6377c8d4358276f
KatsumiOuwa <ouwa@emtechs.co.jp>
2025-06-29 19:47:51 +0900

    Make forward speed configurable

--- etrobo_runner/etrobo_runner.py
+++ etrobo_runner/etrobo_runner.py
@@ -18,6 +18,8 @@ class Runner(Node):
         Proportional gain.
     kd : float
         Derivative gain.
+    speed : float
+        Forward linear velocity.
     """
 
     def __init__(self) -> None:
@@ -38,6 +40,7 @@ class Runner(Node):
         self.threshold = self.declare_parameter('threshold', 0.46).value
         self.kp = self.declare_parameter('kp', -0.64).value
         self.kd = self.declare_parameter('kd', -1.2).value
+        self.speed = self.declare_parameter('speed', 0.05).value
 
         self.timer_period = 0.01
         self.create_timer(self.timer_period, self.publish_cmd_vel)
@@ -64,7 +67,7 @@ class Runner(Node):
         print(f' {self.luminance:.3f}, {error:.3f}, {derivative:.3f}, '
               f'{angular:.3f}')
 
-        twist.linear.x = 0.05
+        twist.linear.x = self.speed
         twist.angular.z = angular
         self.publisher_.publish(twist)
 
